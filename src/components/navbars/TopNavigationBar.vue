f9481d28d2dc5ae6aaf13bf040ac8ef8866b0dc6
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-05-20 07:28:39 +0800

    styles: improve top bar

--- src/components/navbars/TopNavigationBar.vue
+++ src/components/navbars/TopNavigationBar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import SignOutDialog from "@/components/dialogs/SignOutDialog.vue";
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
 import { useAuthStore } from "@/stores/store-auth";
 
 const menuOpen = ref(false);
@@ -16,22 +16,36 @@ onMounted(() => {
   const interval = setInterval(updateTime, 1000);
   onUnmounted(() => clearInterval(interval));
 });
+
+const formattedTime = computed(() => {
+  const parts = currentTime.value.split(' at ');
+  const datePart = parts[0];
+  const timePart = parts[1];
+  const [time, period] = timePart.split(' ');
+
+  return `<strong>${datePart}</strong> at <strong>${time}</strong> ${period}`;
+});
 </script>
 
 <template>
-  <v-app-bar image="/cspc-nabua.jpg" :elevation="1">
+  <v-app-bar image="/cspc-nabua.jpg">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon :ripple="false">
+        <v-icon size="x-large" class="text-white">mdi-book-account</v-icon>
+      </v-app-bar-nav-icon>
+    </template>
     <template v-slot:image>
       <v-img
         gradient="to top right, rgba(19,84,122,.9), rgba(60,62,208,.5)"
       ></v-img>
     </template>
-    <v-app-bar-title class="">
+    <v-app-bar-title>
       <p class="font-weight-bold text-white">Learning Resources and Development Services</p>
       <p class="text-uppercase font-weight-bold text-blue-lighten-1">E-Accession Record Module</p>
     </v-app-bar-title>
 
     <v-app-bar-title class="text-white text-end mr-3">
-      {{ currentTime }}
+      <span v-html="formattedTime"></span>
     </v-app-bar-title>
 
     <v-btn stacked>
