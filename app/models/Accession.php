a73a875c61a6650e925861d85e2df7942052a59d
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-05-20 09:27:55 +0800

    feat: create Accession model

---
+++ app/models/Accession.php
@@ -0,0 +1,213 @@
+<?php
+
+require_once 'App.php';
+
+class Accession extends App
+{
+    // table
+    protected $table = 'accession';
+
+    // properties
+    protected $id;
+    protected $accession_number;
+    protected $date_received;
+    protected $source_of_fund;
+    protected $cost_price;
+    protected $remarks;
+    protected $isbn;
+    protected $dateaccession;
+    protected $title;
+    protected $author;
+    protected $edition;
+    protected $volumes;
+    protected $pages;
+    protected $copyright;
+    protected $publisher;
+    protected $department;
+    protected $copy;
+    protected $encoder;
+    protected $type;
+    protected $publicationPlace;
+    protected $call_no;
+
+    /***************************************************************************
+     * Accession constructor
+     *
+     * @param array $data
+     */
+    public function __construct($data = [])
+    {
+        parent::__construct();
+        $this->fill($data);
+    }
+
+    /***************************************************************************
+     * Fill the properties with data
+     *
+     * @param array $data
+     * @return void
+     */
+    public function fill($data)
+    {
+        foreach ($data as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    /***************************************************************************
+     * Save the record to the database
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        if (empty($this->accession_number)) {
+            self::returnError('HTTP/1.1 400', 'Accession number is required.');
+        }
+
+        $stmt = $this->conn->prepare("SELECT COUNT(*) as count FROM $this->table WHERE accession_number = ?");
+        $stmt->bind_param("s", $this->accession_number);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
+
+        if ($row['count'] > 0) {
+            self::returnError('HTTP/1.1 409', 'Accession number already exists.');
+        }
+
+        $stmt = $this->conn->prepare("INSERT INTO $this->table (accession_number, date_received, source_of_fund, cost_price, remarks, isbn, dateaccession, title, author, edition, volumes, pages, copyright, publisher, department, copy, encoder, type, publicationPlace, call_no) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssssssssssssssss", $this->accession_number, $this->date_received, $this->source_of_fund, $this->cost_price, $this->remarks, $this->isbn, $this->dateaccession, $this->title, $this->author, $this->edition, $this->volumes, $this->pages, $this->copyright, $this->publisher, $this->department, $this->copy, $this->encoder, $this->type, $this->publicationPlace, $this->call_no);
+        return $stmt->execute();
+    }
+
+    /***************************************************************************
+     * Update the record in the database
+     *
+     * @return bool
+     */
+    public function update()
+    {
+        if (empty($this->id)) {
+            self::returnError('HTTP/1.1 400', 'ID is required for update.');
+        }
+
+        $stmt = $this->conn->prepare("SELECT COUNT(*) as count FROM $this->table WHERE id = ?");
+        $stmt->bind_param("i", $this->id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
+
+        if ($row['count'] == 0) {
+            self::returnError('HTTP/1.1 404', 'Record not found.');
+        }
+
+        $stmt = $this->conn->prepare("UPDATE $this->table SET accession_number = ?, date_received = ?, source_of_fund = ?, cost_price = ?, remarks = ?, isbn = ?, dateaccession = ?, title = ?, author = ?, edition = ?, volumes = ?, pages = ?, copyright = ?, publisher = ?, department = ?, copy = ?, encoder = ?, type = ?, publicationPlace = ?, call_no = ? WHERE id = ?");
+        $stmt->bind_param("ssssssssssssssssssssi", $this->accession_number, $this->date_received, $this->source_of_fund, $this->cost_price, $this->remarks, $this->isbn, $this->dateaccession, $this->title, $this->author, $this->edition, $this->volumes, $this->pages, $this->copyright, $this->publisher, $this->department, $this->copy, $this->encoder, $this->type, $this->publicationPlace, $this->call_no, $this->id);
+        return $stmt->execute();
+    }
+
+    /***************************************************************************
+     * Delete the record from the database
+     *
+     * @return bool
+     */
+    public function destroy()
+    {
+        if (empty($this->id)) {
+            self::returnError('HTTP/1.1 400', 'ID is required for deletion.');
+        }
+
+        $stmt = $this->conn->prepare("SELECT COUNT(*) as count FROM $this->table WHERE id = ?");
+        $stmt->bind_param("i", $this->id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $stmt->close();
+
+        if ($row['count'] == 0) {
+            self::returnError('HTTP/1.1 404', 'Record not found.');
+        }
+
+        $stmt = $this->conn->prepare("DELETE FROM $this->table WHERE id = ?");
+        $stmt->bind_param("i", $this->id);
+        return $stmt->execute();
+    }
+
+    /***************************************************************************
+     * Find a record by id
+     *
+     * @param int $id
+     * @return Accession|false
+     */
+    public static function findById($id)
+    {
+        $accession = new Accession();
+        $stmt = $accession->conn->prepare("SELECT * FROM $accession->table WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($row = $result->fetch_assoc()) {
+            return new Accession($row);
+        } else {
+            return false;
+        }
+    }
+
+    /***************************************************************************
+     * Get all records as array of objects
+     *
+     * @return Accession[]
+     */
+    public static function all()
+    {
+        $accession = new Accession();
+        $sql = "SELECT * FROM $accession->table ORDER BY id";
+        $stmt = $accession->conn->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $accessions = [];
+        while ($row = $result->fetch_assoc()) {
+            $accessions[] = new Accession($row);
+        }
+        return $accessions;
+    }
+
+    /***************************************************************************
+     * Get all records as array of arrays
+     *
+     * @return array
+     */
+    public static function rows()
+    {
+        $accessions = [];
+        foreach (self::all() as $accession) {
+            $accessions[] = $accession->toArray();
+        }
+        return $accessions;
+    }
+
+    /***************************************************************************
+     * Convert object to array
+     *
+     * @param array $append
+     * @return array
+     */
+    public function toArray($append = [])
+    {
+        $arr = get_object_vars($this);
+        unset($arr['conn'], $arr['table']);
+
+        // append
+        foreach ($append as $key => $value) {
+            $arr[$key] = $value;
+        }
+
+        return $arr;
+    }
+}
+?>
